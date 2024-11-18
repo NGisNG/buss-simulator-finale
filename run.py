cf7c12ac0b3513120f79912e33cc7dbce3293a91
Parwiz Hussaini <p.syas90@yahoo.com>
2024-11-18 15:43:11 +0000

    Add structure for the run method in the Bus class

--- run.py
+++ run.py
@@ -18,3 +18,23 @@ class Bus:
     """
     def __init__(self):
         self.passengers = []
+    
+     def run(self):
+        """
+        Function to run the bus system.
+        Presents a menu for adding, removing, and viewing passengers, as well as showing statistics.
+        """
+        while True:
+            replit.clear()
+            choice = get_choice_with_arrows(["Add passenger", "Remove passenger", "Show passenger list", "Statistics", "Exit"])
+
+            if choice == "Add passenger":
+                self.add_passenger()
+            elif choice == "Remove passenger":
+                self.remove_passenger()
+            elif choice == "Show passenger list":
+                self.view_passengers()
+            elif choice == "Statistics":
+                self.show_statistics()
+            elif choice == "Exit":
+                break
