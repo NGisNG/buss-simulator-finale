accb9b4669af8c3df0ee648ff8a54092c20d4f80
Parwiz Hussaini <p.syas90@yahoo.com>
2024-11-18 14:39:28 +0000

    Add Passenger class with constructor and basic attributes

---
+++ run.py
@@ -0,0 +1,7 @@
+class Passenger:
+    """A class to represent a passenger."""
+    def __init__(self, age, gender, occupation):
+        """Initialize passenger with age, gender, and occupation."""
+        self.age = age
+        self.gender = gender
+        self.occupation = occupation 
