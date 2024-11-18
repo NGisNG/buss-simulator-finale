5917e43fccd54efb2e805f0c8626cc49d30716f8
Parwiz Hussaini <p.syas90@yahoo.com>
2024-11-18 16:41:18 +0000

    Add structure for add_passenger method in Bus class

--- run.py
+++ run.py
@@ -41,22 +41,43 @@ class Bus:
     def __init__(self):
         self.passengers = []
     
-     def run(self):
-        """
-        Function to run the bus system.
-        Presents a menu for adding, removing, and viewing passengers, as well as showing statistics.
-        """
-        while True:
-            replit.clear()
-            choice = get_choice_with_arrows(["Add passenger", "Remove passenger", "Show passenger list", "Statistics", "Exit"])
+        def run(self):
+            """
+            Function to run the bus system.
+            Presents a menu for adding, removing, and viewing passengers, as well as showing statistics.
+            """
+            while True:
+                replit.clear()
+                choice = get_choice_with_arrows(["Add passenger", "Remove passenger", "Show passenger list", "Statistics", "Exit"])
 
-            if choice == "Add passenger":
-                self.add_passenger()
-            elif choice == "Remove passenger":
-                self.remove_passenger()
-            elif choice == "Show passenger list":
-                self.view_passengers()
-            elif choice == "Statistics":
-                self.show_statistics()
-            elif choice == "Exit":
-                break
+                if choice == "Add passenger":
+                    self.add_passenger()
+                elif choice == "Remove passenger":
+                    self.remove_passenger()
+                elif choice == "Show passenger list":
+                    self.view_passengers()
+                elif choice == "Statistics":
+                    self.show_statistics()
+                elif choice == "Exit":
+                    break
+
+        def add_passenger(self):
+            """
+            Function to add a passenger to the bus.
+            Asks the user for the passenger's age, gender, and occupation and adds them to the passenger list.
+            """
+            while True:
+                replit.clear()
+                age = get_age()
+                gender = get_gender()
+                occupation = get_occupation()
+                replit.clear() 
+                passenger = Passenger(age, gender, occupation)
+                self.passengers.append(passenger)
+                print("Passenger added successfully...")
+                time.sleep(1)
+
+                additional_choice = get_choice_with_arrows(["Return to menu", "Add a new passenger"])
+
+                if additional_choice == "Return to menu":
+                    break
